585f3700dea320bda59c93a547a26cdae2b497a8
Anthony Gladden <a.gladden360@outlook.com>
2022-05-15 16:50:35 -0700

    updated test for save method it passed

--- lib/word.rb
+++ lib/word.rb
@@ -1,9 +1,24 @@
 require('pry')
 
 class Word
-  @@name = {}
+  attr_reader :id, :name
+
+  @@words = {}
+  @@total_row = 0
+
+  def initialize(name, id)
+    @name = name
+    @id = id || @@total_row += 1
+  end
+
+  def save()
+    @@words[self.id] = Word.new(self.name, self.id)
+  end
 
   def self.all()
-    @@name.values
+    @@words.values
   end
-end
+  
+  # def ==(other_words)
+  #   self.name.eql?(other_words.name)
+  end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,7 +1,39 @@
-require('word.rb')
+require('word')
+require('rspec')
+
+describe 'Word' do
+  
+  # before(:each) do
+  #   Album.clear()
+  # end
+
+describe('#save') do
+    it("saves a word") do
+      # words = Word.new("Grant", nil)
+      # words.save()
+      # words2 = Word.new("Bill", nil) 
+      # words2.save()
+      # expect(Word.all).to(eq([words, words2]))
+    end
+  end
 
 describe(".all") do
-  it("is empty at first") do
+  it("return empty array") do
     expect(Word.all()).to(eq([]))
   end
+end
+
+#   describe('#initialize') do
+#     it('it will recongnize name and id') do
+#       words = Word.new('tim')
+#       words = Word.new(nil)
+#     expect(Word.new()).to(eq('tim'))
+#     expect(Word.new()).to(eq(1))
+#   end
+# end
+
+
+
+
+# end
 end
