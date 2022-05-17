2ece990e2b6d719ed00c7314395a73f956ff30f2
Anthony Gladden <a.gladden360@outlook.com>
2022-05-17 15:46:29 -0700

    updated word_intergration_spec.rb for update and delete

--- spec/word_intergration_spec.rb
+++ spec/word_intergration_spec.rb
@@ -3,6 +3,10 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
+before(:each) do
+  Word.clear
+end
+
 describe('create a word path',{:type => :feature}) do
   it('creates a word and goes to word page') do
     visit('/word')
@@ -10,5 +14,32 @@ describe('create a word path',{:type => :feature}) do
     fill_in('word_name', :with => 'Giant')
     click_on('Go!')
     expect(page).to have_content('Giant')
+  end
+end
+
+describe('update a word path', {:type => :feature}) do
+  it('updates a word and returns to page') do
+    Word.clear
+    word = Word.new('Giant',nil)
+    word.save
+    visit('/word')
+    save_and_open_page
+    click_on('Giant')
+    click_on('Edit word')
+    fill_in('name', :with => 'Small')
+    click_on('Update')
+    expect(page).to have_content('Small')
+  end
+end
+
+describe('delete a word path', {:type => :feature}) do
+  it('deletes a word and returns to the words page') do
+    word = Word.new("Giant", nil)
+    word.save
+    visit('/word') 
+    click_on("Giant")
+    click_on("Edit word")
+    click_on("Delete word")
+    expect(page).to have_no_content("Giant")
   end
 end
