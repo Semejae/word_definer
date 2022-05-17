cebb517f7aef24dd275c6bda41e4884e66b4224c
Anthony Gladden <a.gladden360@outlook.com>
2022-05-16 20:08:47 -0700

    added save and clear method and test

--- lib/definition.rb
+++ lib/definition.rb
@@ -17,4 +17,16 @@ class Definition
     (self.name() == definition_to_compare.name()) && (self.word_id() == definition_to_compare.word_id())
   end
 
+  def self.all
+    @@definition.values
+  end
+  
+  def self.clear
+    @@definition = {}
+  end
+
+  def save
+    @@definition[self.id] = Definition.new(self.name, self.word_id, self.id)
+  end
+
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -7,7 +7,7 @@ describe '#definition' do
 
   before(:each) do
     Word.clear()
-    # Definition.clear()
+    Definition.clear()
     @word = Word.new('grant', nil)
     @word.save()
   end
@@ -18,6 +18,35 @@ describe '#definition' do
       definition = Definition.new("grant", @word.id, nil)
       definition2 = Definition.new("grant", @word.id, nil)
       expect(definition).to(eq(definition2))
+    end
+  end
+
+  describe('.all') do
+    it("returns a list of all definition") do
+      definition = Definition.new("Giant", @word.id, nil)
+      definition.save()
+      definition2 = Definition.new("Small", @word.id, nil)
+      definition2.save()
+      expect(Definition.all).to(eq([definition, definition2]))
+    end
+  end
+
+  describe('.clear') do
+    it("clears all definition") do
+      definition = Definition.new("Giant Steps", @word.id, nil)
+      definition.save()
+      definition2 = Definition.new("Naima", @word.id, nil)
+      definition2.save()
+      Definition.clear()
+      expect(Definition.all).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it("saves a defintion") do
+      definition = Definition.new("Big", @word.id, nil)
+      definition.save()
+      expect(Definition.all).to(eq([definition]))
     end
   end
 
